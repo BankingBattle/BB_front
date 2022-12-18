ddb707503a6071faa769f8a58b055117bc596de0
timickb <timickb@yanex.ru>
2022-12-18 03:30:58 +0300

    Added PlayerItem and GameCard components

---
+++ src/components/game_card/index.tsx
@@ -0,0 +1,14 @@
+import { GameResult } from '../../models/GameResult';
+
+interface GameCardProps {
+  result: GameResult
+}
+export function GameCard({ result } : GameCardProps) {
+  return (
+    <div className="rounded-xl bg-amber-500 w-24 p-2 m-2">
+      {result.game.name}
+      &nbsp;
+      {result.result}
+    </div>
+  )
+}
