67a3b39a424e0f3f8923c0ac61dee98e52468a42
timickb <timickb@yanex.ru>
2023-01-18 00:28:37 +0300

    Profile & index

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         changeOrigin: true,
-        target: 'http://localhost:8000',
+        target: 'https://bankingbattle.ru',
       },
     },
   },
