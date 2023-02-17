89079daf9c87ee4b55c24d2975a1609164cfdb08
timickb <timickb@yandex.ru>
2023-02-17 19:09:22 +0300

    added create game butto

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,9 +1,17 @@
 import { GamesList } from '../components/GamesList';
+import { useTranslation } from 'react-i18next';
 
 function Home() {
+  const { t } = useTranslation();
+
   return (
     <>
       <GamesList />
+      <a
+        className="mx-1 my-5 px-3 py-2 rounded-md transition-colors bg-purple-500 text-white block"
+        href="create">
+        + {t('Create game')}
+      </a>
     </>
   );
 }
